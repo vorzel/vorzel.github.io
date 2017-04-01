344a9c75e0acd2df9c8c49bc99d9c6ca0522863b
fools-day <vorzel420@gmail.com>
2017-04-01 11:27:21 +0200

    add first version of joke

---
+++ pages/js/2017-04-01.js
@@ -0,0 +1,11 @@
+function showText(text) {
+        setTimeout(
+            function () {
+                alert(text);
+            },
+            1000
+        );
+}
+
+showText("Happy April Fools\’ Day!");
+
